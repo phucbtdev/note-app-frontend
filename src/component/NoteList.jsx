acfecc1be3802632e49590e6619735a0cc4a8c1a
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-03-05 19:21:32 +0700

    [Component] - Create NoteListe component

---
+++ src/component/NoteList.jsx
@@ -0,0 +1,78 @@
+import { Card, CardContent, Grid, List, ListItem, Typography, Box } from '@mui/material';
+import React from 'react';
+
+const ListNote = () => {
+
+    const notes = [
+        { id: '1', note: 'no dangerousl dangerouslySetInnerHTMLySet dangerouslySetInnerHTMLInnerHTMLte' },
+        { id: '2', note: 'no dangerousl dangerouslySetInnerHTMLySet dangerouslySetInnerHTMLInnerHTMLte' },
+    ]
+    return (
+        <>
+            <Grid height={'100%'} >
+                <Grid item xs={4}
+                    sx={{
+                        width: '100%',
+                        maxWidth: 360,
+                        bgcolor: '#F0EBE3',
+                        height: '100%',
+                        fontWeight: 'bold',
+                        overflowY: 'auto',
+                        padding: '10px',
+                        textAlign: 'left',
+                    }}
+                >
+                    <List
+                        sx={{
+                            overflowY: 'auto',
+                        }}
+
+                        subheader={
+                            <Box>
+                                <Typography sx={{
+                                    fontWeight: 'bold'
+                                }} >
+                                    Notes
+                                </Typography>
+                            </Box>
+                        }
+                    >
+                        {notes.map(({ id, note }) => {
+                            return (
+                                <ListItem key={id}
+                                >
+                                    <Card
+                                        sx={{
+                                            cursor: 'pointer'
+                                        }}
+                                    >
+                                        <CardContent
+                                            sx={{ '&:last-child': { pb: '10px' }, padding: '10px' }}
+                                        >
+                                            <div
+                                                style={{ fontSize: 14, fontWeight: 'bold' }}
+                                                dangerouslySetInnerHTML={{
+                                                    __html: `${note.substring(0, 30) || 'Empty'}`,
+                                                }}
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </ListItem>
+                            )
+                        })}
+
+                    </List>
+                </Grid>
+
+                <Grid item xs={8}>
+
+                </Grid>
+
+            </Grid>
+
+
+        </>
+    );
+}
+
+export default ListNote;
